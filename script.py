e2387bab4cbe65f0b4871ac252ed256481eacaa6
AndySanchezA <76460870+AndySanchezA@users.noreply.github.com>
2020-12-22 13:51:15 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,11 @@
+# Script de prueba para Git
+# Autor: Rodrigo Tufiño <rtufino@ups.edu.ec> # Fecha: 23-06-2020
+# Versión: 1.0.0
+name ="main"
+def main():
+    nombre = input("Cual es tu nombre?")
+    print("Hola",nombre,"Bienvenido a Git")
+
+
+if  name == "main":
+    main()
